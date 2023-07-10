5cb8b7db2cb31bb153901284367573ef3187f543
Mohana-Sowdesh <125632241+Mohana-Sowdesh@users.noreply.github.com>
2023-07-10 15:00:59 +0530

    Create Product to a single digit using recursion.cs

---
+++ Week 1/Day 3/Product to a single digit using recursion.cs
@@ -0,0 +1,22 @@
+using System;
+
+class HelloWorld {
+  static int CalcProduct(int number) {
+    int product = 1;
+    while(number>0) {
+        int temp = number%10;
+        number /=10;
+        product = product*temp;
+    }
+    if(product>=10)
+        return CalcProduct(product);
+    return product;
+  }
+  
+  static void Main() {
+    Console.WriteLine("Enter a number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int result = CalcProduct(num);
+    Console.WriteLine(result);
+  }
+}
