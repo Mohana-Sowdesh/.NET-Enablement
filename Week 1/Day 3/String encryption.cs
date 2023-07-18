7da43219d61fa179accf56f05d0ec6bc41f43e49
Mohana-Sowdesh <125632241+Mohana-Sowdesh@users.noreply.github.com>
2023-07-18 14:03:05 +0530

    Update String encryption.cs

--- Week 1/Day 3/String encryption.cs
+++ Week 1/Day 3/String encryption.cs
@@ -1,33 +1,30 @@
 using System;
+using System.Text;
 
 class Encryption {
     static void Main() {
         Console.WriteLine("Enter the no. of characters: ");
-        int usr_input = Convert.ToInt32(Console.ReadLine());
+        int usrInput = Convert.ToInt32(Console.ReadLine());
         StringBuilder stringBuilder = new StringBuilder();
 
-        while(usr_input!=0) {
+        for( ; usrInput!=0; ) {
             char character = char.Parse(Console.ReadLine());
-            int ascii = charToInt(character);
-            int first_digit = 0, temp = ascii, rem = 0;
-            while(temp!=0) {
-                rem = temp%10;
-                temp = temp/10;
+            int ascii = (int)(character);
+            int firstDigit = 0, temp = ascii, rem = 0;
+            for( ; temp != 0; ) {
+                rem = temp % 10;
+                temp = temp / 10;
             }
-            first_digit = rem;
-            int last_digit = ascii%10;
+            firstDigit = rem;
+            int lastDigit = ascii % 10;
 
-            int first_part = ascii+last_digit;
-            string middle_part = first_digit.ToString()+last_digit.ToString();
-            int last_part = ascii-first_digit;
+            int firstPart = ascii + lastDigit;
+            string middlePart = (firstDigit * 10 + lastDigit).ToString();
+            int lastPart = ascii - firstDigit;
 
-            stringBuilder.Append((char)first_part + middle_part + (char)last_part);
-            usr_input--;
+            stringBuilder.Append((char)firstPart + middlePart + (char)lastPart);
+            usrInput--;
         }
         Console.WriteLine(stringBuilder.ToString());
     }
-
-    static int charToInt(char character) {
-        return (int)character;
-    } 
 }
