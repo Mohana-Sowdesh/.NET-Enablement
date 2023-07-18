3134a5285f6d5e50da35c06b26b966f4d457c340
Mohana-Sowdesh <125632241+Mohana-Sowdesh@users.noreply.github.com>
2023-07-18 14:00:29 +0530

    Update Sum of numbers in a matrix.cs

--- Week 1/Day 3/Sum of numbers in a matrix.cs
+++ Week 1/Day 3/Sum of numbers in a matrix.cs
@@ -1,35 +1,33 @@
-using System;
-
 class HelloWorld {
   static void Main() {
     Console.WriteLine("Enter the number: ");
-    int usr_input = Convert.ToInt32(Console.ReadLine());
+    int usrInput = Convert.ToInt32(Console.ReadLine());
 
-    int[,] result_arr = new int[3,3];
+    int[,] resultArray = new int[3,3];
 
-    result_arr[0,0] = 1;
+    resultArray[0,0] = 1;
 
-    float div_by_3_res = (float)usr_input/3;
-    int div_by_3 = usr_input/3;
-    div_by_3_res = div_by_3_res - div_by_3;
+    float divBy3Res = (float)usrInput/(float)3;
+    int divBy3 = usrInput/3;
+    divBy3Res = divBy3Res - divBy3;
 
-    if(div_by_3_res < 0.5)
-        result_arr[0,1] = div_by_3-1;
+    if(divBy3Res < 0.5)
+        resultArray[0,1] = divBy3 - 1;
     else
-        result_arr[0,1] = div_by_3;
+        resultArray[0,1] = divBy3;
 
-    result_arr[0,2] = usr_input - (result_arr[0,1]+result_arr[0,0]);
+    resultArray[0,2] = usrInput - (resultArray[0,1] + resultArray[0,0]);
 
     for(int i=1; i<3; i++) {
         for(int j=0; j<3; j++) {
-            result_arr[i,j] = result_arr[0,(i+j)%3];
+            resultArray[i,j] = resultArray[0,(i+j)%3];
         }
     }
 
     Console.WriteLine("The result is: ");
     for(int i=0; i<3; i++) {
         for(int j=0; j<3; j++) {
-            Console.Write(result_arr[i,j]+ " ");
+            Console.Write(resultArray[i,j]+ " ");
         }
         Console.WriteLine();
     }
