2b9bca68a2e6889eb9fe7c676393e68ccb721df8
Mohana-Sowdesh <125632241+Mohana-Sowdesh@users.noreply.github.com>
2023-07-18 12:41:30 +0530

    Update Pattern.cs

--- Week 1/Day 2/Pattern.cs
+++ Week 1/Day 2/Pattern.cs
@@ -12,20 +12,21 @@ Output:
 *********
 
 */
+
 using System;
 class HelloWorld {
   static void Main() {
     Console.WriteLine("Please enter the number of lines: ");
     int lines = Convert.ToInt32(Console.ReadLine());
-    int i=0,j=0,k=0;
     
-    for(i=1; i<=lines; i++)
+    for(int i=0; i<lines; i++)
     {
-        for(j=1; j<=lines-i; j++)
+        int j = 0;
+        for(j=0; j<lines-i-1; j++)
         {
             Console.Write(" ");
         }
-        for(k=1; k<=(2*i)-1; k++)
+        for(j=0; j<(2*i)+1; j++)
         {
             Console.Write("*");
         }
