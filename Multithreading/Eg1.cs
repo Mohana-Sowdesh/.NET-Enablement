8567e5e7c224d0ee96a068a9d2773c20a53936bf
Mohana-Sowdesh <125632241+Mohana-Sowdesh@users.noreply.github.com>
2023-07-21 11:32:19 +0530

    Create Eg1.cs

---
+++ Multithreading/Eg1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+ 
+public class MXThread {
+ 
+    // Non-static method
+    public void mythr()
+    {
+        for (int j = 0; j < 2; j++) {
+ 
+            Console.WriteLine("My Thread is in progress...!!");
+        }
+    }
+}
+ 
+// Driver Class
+public class GFG {
+ 
+    // Main Method
+    public static void Main()
+    {
+        // Creating object of ExThread class
+        MXThread obj = new MXThread();
+ 
+        // Creating and initializing thread
+        // Using thread class and
+        // ThreadStart constructor
+        Thread t = new Thread(new ThreadStart(obj.mythr));
+        t.Start();
+    }
+}
